c006f5d4e61234b4405045e6cfce8fe55d013aaf
OGEmanuel <emanuolumide@mail.com>
2022-12-07 09:26:03 +0100

    Dashboard Layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import './App.css';
 import Signin from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
 
@@ -7,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Signin/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route exact path="/" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
