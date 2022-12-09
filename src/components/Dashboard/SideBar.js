1d4dbba84d6ab804fbd7ed1031672e6d04186364
OGEmanuel <1blacksanta416@gmail.com>
2022-12-09 13:38:29 +0100

    sidenav UI

--- src/components/Dashboard/SideBar.js
+++ src/components/Dashboard/SideBar.js
@@ -1,10 +1,64 @@
-import { Fragment } from "react";
+import { useState } from "react";
+import dashboard from "../../assets/dashboard.svg";
+import dashboardColor from "../../assets/dashboardColor.svg";
+import assessments from "../../assets/assessments.svg";
+import notification from "../../assets/notification.svg";
+import logout from "../../assets/logout.svg";
+
+import classes from "./SideBar.module.css";
 
 const SideBar = () => {
+  // const [visited, setVisited] = useState(false);
+
+  // const handleClick = (e) => {
+  //   // const active = e.target;
+  //   // if (active) {
+  //   //   setVisited(true);
+  //   //   return;
+  //   // }
+
+  //   console.log("clicked");
+  // };
+
   return (
-    <Fragment>
-      <p>Sidebar</p>
-    </Fragment>
+    <div className={classes.sidebar}>
+      <div className={classes.navigate}>
+        <a
+          href="#"
+          // onClick={handleClick}
+          // id="1"
+          className={classes.link}
+        >
+          <img src={dashboard} alt="" />
+          <p>Dashboard</p>
+        </a>
+        <a
+          href="#"
+          // onClick={handleClick}
+          // id="2"
+          className={classes.link}
+        >
+          <img src={assessments} alt="" />
+          <p>Assessments</p>
+        </a>
+        <a
+          href="#"
+          // onClick={handleClick}
+          // id="3"
+          className={classes.link}
+        >
+          <img src={notification} alt="" />
+          <p>Notifications</p>
+        </a>
+      </div>
+
+      <div className={classes["log-out"]}>
+        <a href="#" className={classes.link}>
+          <img src={logout} alt="" />
+          <p>Log out</p>
+        </a>
+      </div>
+    </div>
   );
 };
 
