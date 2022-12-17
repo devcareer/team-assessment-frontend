4bb37d071d43c44e7a60b6f010ea84c119addf9c
OGEmanuel <emanuolumide@mail.com>
2022-12-17 13:46:11 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import Signin from './pages/Signin';
 import Home from './pages/Home';
-import NavBar from './components/Home/NavBar';
-import SideBar from './components/Home/SideBar';
+// import NavBar from './components/Home/NavBar';
+// import SideBar from './components/Home/SideBar';
 
 function App() {
   return (
