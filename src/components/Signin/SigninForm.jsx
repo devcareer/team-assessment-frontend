4d0af04a02675980a9d61963e9c4e61bbf6fb9b8
OGEmanuel <1blacksanta416@gmail.com>
2023-01-09 16:52:50 +0100

    revamping

--- src/components/Signin/SigninForm.jsx
+++ src/components/Signin/SigninForm.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import google from '../../assets/google-icon.png';
-import chrome from '../../assets/chrome.png';
-import firefox from '../../assets/firefox.png';
-import explorer from '../../assets/explorer.png';
-import './SigninForm.css';
+// import chrome from '../../assets/chrome.png';
+// import firefox from '../../assets/firefox.png';
+// import explorer from '../../assets/explorer.png';
+import classes from './SigninForm.module.css';
+import password from '../../assets/password.svg';
 
 function SigninForm() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -45,77 +46,72 @@ function SigninForm() {
   };
 
   return (
-    <div className="form flex flex-row justify-center p-10">
-      <div className="w-1/2">
-        <img className="" src={logo} alt="logo" />
-        <div className="w-3/4 mx-auto p-16">
-          <button className="google-button py-1 flex flex-row mx-auto justify-center items-center text-base">
-            <img className="w-7 mr-3" src={google} alt="" />
-            Sign in with Google
-          </button>
-          <div className="or flex flex-row justify-center items-center py-10">
-            <hr className="w-1/2 mr-3" />
-            OR
-            <hr className="w-1/2 ml-3" />
-          </div>
-          <form>
-            <div className="">
-              <label className="font-medium text-black" htmlFor="name">
-                Full Name<span className="ml-1 text-red-600">*</span>
-              </label>
-              <br />
-              <input
-                className="input mx-auto border text-sm border-gray-300 p-4 rounded-lg"
-                type="text"
-                id="name"
-                value={enteredName}
-                onChange={nameChangeHandler}
-                placeholder="e.g Chukwuma Adekunle Ciroma"
-                required
-              />
-              <br />
-              <label className="font-medium text-black" htmlFor="email">
-                Email Address<span className="ml-1 text-red-600">*</span>
-              </label>
-              <br />
-              <input
-                className="input mx-auto border text-sm border-gray-300 p-4 rounded-lg"
-                type="email"
-                id="email"
-                value={enteredEmail}
-                onChange={emailChangeHandler}
-                placeholder="e.g chukwuciro@gmail.com"
-                required
-              />
-              <br />
-              <div className="flex flex-row justify-start items-start">
-                <input
-                  className="mr-2 mt-1"
-                  type="checkbox"
-                  required
-                  onChange={checkHandler}
-                />
-                <div className="">
-                  I agree to the processing of my personal data (name and email)
-                  for the purpose of conducting the assessment. Read{' '}
-                  <a href="/" className="policy hover:underline">
-                    Privacy Policy
-                  </a>{' '}
-                  to know more.
-                </div>
-              </div>
-              <button
-                className="button py-2 font-medium"
-                disabled={!formIsValid}
-                onClick={submitHandler}
-              >
-                Sign In
-              </button>
-            </div>
-          </form>
+    // <div className="form flex flex-row justify-center p-10">
+    <div className={classes.form}>
+      <img src={logo} alt="logo" />
+      <div className={classes['form-container']}>
+        <button>
+          <img src={google} alt="google" />
+          Sign in with Google
+        </button>
+        <div className={classes.break}>
+          <hr />
+          OR
+          <hr />
         </div>
+        <form>
+          {/* <div className=""> */}
+          <label className="" htmlFor="name">
+            Full Name <span>*</span>
+            <input
+              type="text"
+              id="name"
+              value={enteredName}
+              onChange={nameChangeHandler}
+              placeholder="e.g Chukwuma Adekunle Ciroma"
+              required
+            />
+          </label>
+          <label className={classes.password} htmlFor="email">
+            Password <span>*</span>
+            <br />
+            <input
+              type="password"
+              id="password"
+              value={enteredEmail}
+              onChange={emailChangeHandler}
+              placeholder="password"
+              required
+            />
+            <img src={password} alt="" />
+          </label>
+          <div className="">
+            <label htmlFor="">
+              <input
+                className=""
+                type="checkbox"
+                required
+                onChange={checkHandler}
+              />
+              <p>
+                I agree to the processing of my personal data (name and email)
+                for the purpose of conducting the assessment. Read{' '}
+                <a href="/" className="">
+                  Privacy Policy
+                </a>
+                to know more.
+              </p>
+            </label>
+          </div>
+          <button className="" disabled={!formIsValid} onClick={submitHandler}>
+            Sign In
+          </button>
+          {/* </div> */}
+        </form>
       </div>
-      <div className="sign-text text-white w-1/2 flex justify-center items-center">
+    </div>
+
+    /* <div className="sign-text text-white w-1/2 flex justify-center items-center">
         <div className="w-3/4 normal">
           <div className="font-medium text-3xl mb-1">
             Give your best shot while answering the questions
@@ -136,8 +132,8 @@ function SigninForm() {
             enable you to login within seconds.
           </div>
         </div>
-      </div>
-    </div>
+      </div> */
+    // </div>
   );
 }
 
