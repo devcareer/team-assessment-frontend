f1961d5765aa92bd186091f8f0501c2defbf60e2
OGEmanuel <emanuolumide@mail.com>
2023-01-09 23:47:47 +0100

    Sign In logic under cinstruction

--- src/components/Signin/SigninForm.jsx
+++ src/components/Signin/SigninForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import useInput from '../../hooks/use-input';
 import logo from '../../assets/logo.png';
 import google from '../../assets/google-icon.png';
 // import chrome from '../../assets/chrome.png';
@@ -8,41 +10,57 @@ import google from '../../assets/google-icon.png';
 import classes from './SigninForm.module.css';
 import password from '../../assets/password.svg';
 
+const validInput = value => value.trim().length > 6;
+
 function SigninForm() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredName, setEnteredName] = useState('');
-  const [checked, setChecked] = useState(false);
-  const [formIsValid, setFormIsValid] = useState(false);
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredName, setEnteredName] = useState('');
+  // const [checked, setChecked] = useState(false);
+  // const [formIsValid, setFormIsValid] = useState(false);
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput(validInput);
 
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      setFormIsValid(
-        checked && enteredName.trim().length > 6 && enteredEmail.includes('@')
-      );
-    }, 500);
+  const {
+    value: enteredPassword,
+    isValid: enteredPasswordIsValid,
+    hasError: passwordInputHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+  } = useInput(validInput);
 
-    return () => {
-      clearTimeout(identifier);
-    };
-  }, [checked, enteredName, enteredEmail]);
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredPasswordIsValid) {
+    formIsValid = true;
+  }
 
   const navigate = useNavigate();
 
-  const emailChangeHandler = e => {
-    setEnteredEmail(e.target.value);
-  };
+  // const emailChangeHandler = e => {
+  //   setEnteredEmail(e.target.value);
+  // };
 
-  const nameChangeHandler = e => {
-    setEnteredName(e.target.value);
-  };
+  // const nameChangeHandler = e => {
+  //   setEnteredName(e.target.value);
+  // };
 
-  const checkHandler = e => {
-    setChecked(e.target.checked);
-  };
+  // const checkHandler = e => {
+  //   setChecked(e.target.checked);
+  // };
 
   const submitHandler = e => {
     e.preventDefault();
-    if (formIsValid) navigate('/home/dashboard');
+    // if (!formIsValid) return;
+    // navigate('/home/dashboard');
+    console.log(enteredName && enteredPassword);
+
+    resetNameInput();
   };
 
   return (
@@ -59,27 +77,37 @@ function SigninForm() {
           OR
           <hr />
         </div>
-        <form>
-          {/* <div className=""> */}
+        <form onSubmit={submitHandler}>
           <label className="" htmlFor="name">
             Full Name <span>*</span>
+            {nameInputHasError && (
+              <p className={classes['error-text']}>
+                Name cannot be blank and must be 6 characters long!
+              </p>
+            )}
             <input
               type="text"
               id="name"
               value={enteredName}
               onChange={nameChangeHandler}
+              onBlur={nameBlurHandler}
               placeholder="e.g Chukwuma Adekunle Ciroma"
               required
             />
           </label>
           <label className={classes.password} htmlFor="email">
             Password <span>*</span>
-            <br />
+            {passwordInputHasError && (
+              <p className={classes['error-text']}>
+                Password must be 6 characters long!
+              </p>
+            )}
             <input
               type="password"
               id="password"
-              value={enteredEmail}
-              onChange={emailChangeHandler}
+              value={enteredPassword}
+              onChange={passwordChangeHandler}
+              onBlur={passwordBlurHandler}
               placeholder="password"
               required
             />
@@ -91,7 +119,7 @@ function SigninForm() {
                 className=""
                 type="checkbox"
                 required
-                onChange={checkHandler}
+                // onChange={checkHandler}
               />
               <p>
                 I agree to the processing of my personal data (name and email)
@@ -103,10 +131,7 @@ function SigninForm() {
               </p>
             </label>
           </div>
-          <button className="" disabled={!formIsValid} onClick={submitHandler}>
-            Sign In
-          </button>
-          {/* </div> */}
+          <button type="submit">Sign In</button>
         </form>
       </div>
     </div>
